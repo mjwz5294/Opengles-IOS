b222f7a9c5aa80350fcc914813d003c10b174443
“mjwz5294” <393578079@qq.com>
2018-12-12 14:21:07 +0800

    Initial Commit

---
+++ Opengles-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Opengles-IOS
+//
+//  Created by mac on 2018/12/12.
+//  Copyright © 2018 mjwz5294. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Opengles-IOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Opengles-IOS
+//
+//  Created by mac on 2018/12/12.
+//  Copyright © 2018 mjwz5294. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
